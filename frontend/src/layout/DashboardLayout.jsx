2f8defa318f09bc6814b4a4ab4f585ef968443bb
Pramudi Vihanga <84280960+Pramudi218@users.noreply.github.com>
2022-08-21 20:49:24 -0700

    Corrected the faults of the profile button in dashboard (#29)

--- frontend/src/layout/DashboardLayout.jsx
+++ frontend/src/layout/DashboardLayout.jsx
@@ -18,6 +18,22 @@ export default function DashboardLayout({ sidebarButtons, children }) {
     navigate('/user-profile');
   }
 
+  function setUserRole(role) {
+    switch (role) {
+      case 'ADMIN':
+        return 'Administrator';
+      case 'CONTROL_OFFICER':
+        return 'Control Officer';
+
+      case 'TICKETING_OFFICER':
+        return 'Ticketing Officer';
+      case 'PASSENGER':
+        return 'Passenger';
+      default:
+        return;
+    }
+  }
+
   return (
     <div className="bg-slate-700 min-w-full min-h-screen flex">
       <nav className="min-w-fit bg-slate-700 min-h-full flex flex-col justify-between">
@@ -32,22 +48,24 @@ export default function DashboardLayout({ sidebarButtons, children }) {
           <div className="flex flex-col">
             {sidebarButtons.map((SidebarButton) => SidebarButton)}
           </div>
-        </div>
-        <div className="self-center flex items-center justify-center bg-slate-900 p-2 w-fit rounded-full mt-8 mb-32 hover:bg-slate-800">
-          <img src={Logo} alt="Profile Icon" className="h-14 w-14" />
+          <div className="ml-4">
+            <div className="self-center flex items-center justify-center bg-slate-900 p-2 w-fit rounded-full mt-8 mb-32 hover:bg-slate-800">
+              <img src={Logo} alt="Profile Icon" className="h-14 w-14" />
 
-          <div
-            onClick={HandleUserProfileView}
-            className="text-slate-200 ml-4 mr-2 text-xs w-28 cursor-pointer"
-          >
-            <div>
-              {currentUser?.firstName} {currentUser?.lastName}
+              <div
+                onClick={HandleUserProfileView}
+                className="text-slate-200 ml-4 mr-2 text-xs w-28 cursor-pointer"
+              >
+                <div>
+                  {currentUser?.firstName} {currentUser?.lastName}
+                </div>
+                <div className="mt-1">{setUserRole(currentUser?.role)}</div>
+              </div>
+              <IconButton aria-label="Logout" onClick={handleLogout}>
+                <LogoutIcon className="text-slate-200" />
+              </IconButton>
             </div>
-            <div className="mt-1">{currentUser?.role}</div>
           </div>
-          <IconButton aria-label="Logout" onClick={handleLogout}>
-            <LogoutIcon className="text-slate-200" />
-          </IconButton>
         </div>
       </nav>
       <main className="bg-slate-100 min-h-full w-full m-5 ml-0 p-10 rounded-3xl">
